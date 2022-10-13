0dd168194b63df059be6aca618586109694f223a
uddinburhan321 <uddinburhan321n@gmail.com>
2022-10-12 23:17:28 -0400

    Solved FindMissingNumber under math_problems

--- src/math_problems/FindMissingNumber.java
+++ src/math_problems/FindMissingNumber.java
@@ -1,13 +1,35 @@
 package math_problems;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class FindMissingNumber {
 
-    /** INSTRUCTIONS
+    /**
+     * INSTRUCTIONS
      * Write a method to find the missing number from the array.
      */
 
     public static void main(String[] args) {
-        int[] array = new int[] {10, 2, 1, 4, 5, 3, 7, 8, 6};
+
+        int[] array = new int[]{10, 2, 1, 4, 5, 3, 7, 8, 6}; //{10, 2, 1, 4, 5, 3, 7, 8, 6};
+
+        System.out.println("Missing number from the array is: " + missingNumberFinder(array));
+    }
+
+    static int missingNumberFinder(int[] arr) {
+
+        int num = 0;
+        for (int i = 0; i < arr.length; i++) {
+            num = num + arr[i];
+        }
+        int expNum = 0;
+        for (int j = 1; j <= 10; j++) {
+            expNum = expNum + j;
+        }
+        int mNum = expNum - num;
+        return mNum;
 
     }
 }
