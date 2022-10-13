7bcb15d9eb566f18cfa7c1c065e7f5fcd11eafe2
uddinburhan321 <uddinburhan321n@gmail.com>
2022-10-13 01:54:44 -0400

    Solved PrimeNumber under math_problems

--- src/math_problems/PrimeNumber.java
+++ src/math_problems/PrimeNumber.java
@@ -1,17 +1,29 @@
 package math_problems;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
 public class PrimeNumber {
 
-    /** INSTRUCTIONS
+    /**
+     * INSTRUCTIONS
      * Write a method to print a list of all prime numbers from 2 to 1,000,000.
-     *  Print out the prime numbers in the given range.
-     *
+     * Print out the prime numbers in the given range.
+     * <p>
      * BONUS: Figure out how to improve algorithmic efficiency
      */
 
-
     public static void main(String[] args) {
-
+        primeNumbersList();
     }
 
+    static void primeNumbersList() {
+
+        List<Integer> primeNumber = new ArrayList<>();
+        IntStream.range(2, 1_000_000)
+                .filter(n -> primeNumber.parallelStream().noneMatch(p -> n % p == 0))
+                .forEach(primeNumber::add);
+        System.out.println("Prime numbers are: " + primeNumber);
+    }
 }
