98de56fa2e89434f0ceedc3ec569a20b03d24509
uddinburhan321 <uddinburhan321n@gmail.com>
2022-10-13 13:39:05 -0400

    Solved DuplicateWord under string_problems

--- src/string_problems/DuplicateWord.java
+++ src/string_problems/DuplicateWord.java
@@ -2,7 +2,8 @@ package string_problems;
 
 public class DuplicateWord {
 
-    /** INSTRUCTIONS
+    /**
+     * INSTRUCTIONS
      * Write a java program to find duplicate words and the number of occurrences of those words in the given string.
      * Also, find the average length of all the words
      */
@@ -12,6 +13,23 @@ public class DuplicateWord {
 
         // Implement here
 
-    }
+        int count;
+        st = st.toUpperCase();
+        String words[] = st.replaceAll("[.|,]", "").split(" ");
 
+        System.out.println("Duplicate words and their number of occurrences : \n");
+
+        for (int i = 0; i < words.length; i++) {
+            count = 1;
+            for (int j = i + 1; j < words.length; j++) {
+                if (words[i].equals(words[j])) {
+                    count++;
+                    words[j] = "0";
+                }
+            }
+            if (count > 1 && words[i] != "0")
+                System.out.println("## \"" + words[i] + "\" duplicated " + count + " Times");
+        }
+        System.out.println("\n## Average length of the words : " + words.length);
+    }
 }
