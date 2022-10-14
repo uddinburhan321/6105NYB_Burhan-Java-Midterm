12f9f2a2347d7829b7589094d1b19b8fda820976
uddinburhan321 <uddinburhan321n@gmail.com>
2022-10-14 03:34:13 -0400

    Completed UseQueue under data_structures

--- src/data_structures/UseQueue.java
+++ src/data_structures/UseQueue.java
@@ -1,5 +1,12 @@
 package data_structures;
 
+import databases.SharedStepsDatabase;
+
+import java.sql.SQLException;
+import java.util.ArrayDeque;
+import java.util.List;
+import java.util.Queue;
+
 public class UseQueue {
 
     /** INSTRUCTIONS
@@ -12,6 +19,42 @@ public class UseQueue {
 
 
     public static void main(String[] args) {
+
+        SharedStepsDatabase ssdb = new SharedStepsDatabase();
+        Queue<Object> myQueue = new ArrayDeque<>();
+
+        myQueue.add("Viktoryia");
+        myQueue.add("Chebrine");
+        myQueue.add("Fatema");
+
+        for (Object s : myQueue) {
+            System.out.println(s);
+        }
+
+        ssdb.insertQueue("student", "first_name", myQueue);
+
+        String query = "SELECT * FROM STUDENT";
+
+        List<String> results = null;
+        try {
+            results = ssdb.executeQueryReadAllSingleColumn(query, "first_name");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println();
+        System.out.println("Printing from the database: ");
+
+        for (String s : results) {
+            System.out.println(s);
+        }
+
+        ssdb.closeResources();
+
+        System.out.println();
+        System.out.println("Executing poll() method: " + myQueue.poll());
+        System.out.println("\nExecuting peek() method: " + myQueue.peek());
+        System.out.println("\nExecuting remove() method: " + myQueue.remove());
 
     }
 
